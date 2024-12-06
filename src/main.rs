c5500efdc6383f1e7534810adae8f9bbc38317ce
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2024-12-06 17:29:50 -0500

    printing httprequest

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
+    // create socket, bind to in
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    // listen on the socket
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established!")
+        // handle stream coming from the connection
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // turn TCP Stream into a buffered reader
+    let buf_reader = BufReader::new(&stream);
+
+    // split each line of the request into a vectory until line is empty aka \n\n
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
