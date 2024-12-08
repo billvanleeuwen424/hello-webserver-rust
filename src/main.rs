6f2d203eec4e92cdb5432532990ddf34f720621a
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2024-12-08 17:54:45 -0500

    comment fixing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
 use hello_webserver::ThreadPool;
 
 fn main() {
-    // create socket, bind to in
+    // create socket, bind to it
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     // handle up to 4 requests at once
@@ -25,7 +25,7 @@ fn handle_connection(mut stream: TcpStream) {
     // turn TCP Stream into a buffered reader
     let buf_reader = BufReader::new(&stream);
 
-    // do some unwrap magic to get the first line of the request
+    // Get the first line of the bufreader
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "hello.html")
@@ -33,6 +33,7 @@ fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
+    // read in the file, format response and send it up socket
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
